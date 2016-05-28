dc6f9ee99ae5b95ee136fc3e96305ae1d4b9ab1c
Shipra Sinha <shipra.juet@gmail.com>
2016-05-28 15:15:22 +0530

    Update the webhook API to look for special messages to trigger the cards

--- index.js
+++ index.js
@@ -114,3 +114,20 @@ function sendGenericMessage(sender) {
         }
     })
 }
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
